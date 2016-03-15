b64373148f9080f193c5159bd8600d623b591946
ravendyg <jaroslav.dashkevich@gmail.com>
2016-03-15 21:33:36 +0600

    Clean gulpfile and outdated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,46 +3,19 @@ var gulp = require('gulp'),
     LessPluginCleanCSS = require('less-plugin-clean-css'),
     cleancss = new LessPluginCleanCSS({advanced: true}),
     autoprefixer = require('gulp-autoprefixer'),
-    // minifycss = require('gulp-minify-css'),
-    // // // eslint = require('gulp-eslint'),
-    // jshint = require('gulp-jshint'),
-    // stylish = require('jshint-stylish'),
-    // uglify = require('gulp-uglify'),
-    // rename = require("gulp-rename"),
-    // processhtml = require('gulp-processhtml'),
-    // usemin = require('gulp-usemin'),
     imagemin = require('gulp-imagemin'),
-    // // // rename = require('gulp-rename'),
-    // // concat = require('gulp-concat'),
-    // // notify = require('gulp-notify'),
     cache = require('gulp-cache'),
-    // // // changed = require('gulp-changed'),
-    // rev = require('gulp-rev'),
-    // // // browserSync = require('browser-sync'),
-    del = require('del')
-    // // exec = require('gulp-exec'),
-    // ts = require('gulp-typescript'),
-    // sourcemaps = require('gulp-sourcemaps'),
-    // mocha = require('gulp-mocha')
-    ;
+    del = require('del');
 
-
-// gulp.task('default', ['watch']);
 // watch
 gulp.task('watch', function () {
-// //    gulp.watch('src/scripts/app.ts', ['compileTs']);
    gulp.watch('src/styles/*.less', ['styles']);
-//    gulp.watch('build/scripts/*.js', ['moveTest']);
 });
 
 // build
 gulp.task('build', [`clean`], function () {
     gulp.start('copy', 'usemin', 'imagemin');
 });
-
-
-
-
    
 // styles
 gulp.task('styles', function () {
@@ -53,17 +26,6 @@ gulp.task('styles', function () {
 		.pipe(autoprefixer({browsers: ['last 2 Chrome versions']}))
 		.pipe(gulp.dest('build/styles'));		
 }); 
-
-// // remove test from build
-// gulp.task('cleanTest', ['moveTest'], function () {
-//     return del('build/scripts/test*');
-// });
-// // copy test into separate directory
-// gulp.task('moveTest', function () {
-//    return gulp.src('build/scripts/test*.js')
-//         .pipe(gulp.dest('tests'));
-// });
-
 
 // images
 gulp.task('imagemin', function () {
@@ -77,9 +39,6 @@ gulp.task('imagemin', function () {
 // minimize & uglify
 gulp.task('usemin', function () {
     return gulp.src('src/html/*')
-    //         .pipe(usemin({
-    //             js: [uglify()]
-    //         }))
             .pipe(gulp.dest('build/html'));
 });
 
